aea7f1e17376aee022757574e44c7ba377bdcab6
Devesh Malani <deveshmalani@gmail.com>
2024-05-15 15:15:16 +0530

    Loan Management System

--- Util/PropertyUtil.py
+++ Util/PropertyUtil.py
@@ -4,8 +4,8 @@ class PropertyUtil:
 
     @staticmethod
     def getPropertyString():
-        SERVER_NAME = "Mathesh-PC"
-        DATABASE_NAME = "LoanManager"
+        SERVER_NAME = "DM"
+        DATABASE_NAME = "LoanManagementSystem1"
         TRUSTED_CONNECTION = "Yes"
         CONNECTION_STRING = f"Driver={{SQL Server}}; Server={SERVER_NAME}; Database={DATABASE_NAME}; Trusted_Connection={TRUSTED_CONNECTION}"
         return CONNECTION_STRING
